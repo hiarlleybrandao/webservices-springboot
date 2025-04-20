17f903d9d8ce42c487241123db313aa4a3dd47e6
Hiarlley Brandão <hiarlleybrandao@gmail.com>
2025-04-20 12:33:34 -0300

    Projeto criado com SpringBoot

---
+++ src/main/java/com/brandaostartup/webservicesSB/WebservicesSbApplication.java
@@ -0,0 +1,13 @@
+package com.brandaostartup.webservicesSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandaostartup/webservicesSB/WebservicesSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandaostartup.webservicesSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
