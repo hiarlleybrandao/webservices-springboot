8e0a296f0294a59c1e2aee0ba2e199dd3fe190e6
hiarlleybrandao <hiarlleybrandao@gmail.com>
2025-04-21 21:06:16 -0300

    feat: adicionando camada de servico e component registration

--- src/main/java/com/brandaostartup/webservicesSB/repositories/UserRepository.java
+++ src/main/java/com/brandaostartup/webservicesSB/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.brandaostartup.webservicesSB.repositories;
 
 import com.brandaostartup.webservicesSB.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
